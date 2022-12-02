6e367537a69d12c26506ca04551f6e4f9bbf6ff2
Kevin Hoercher <khoerch@gmail.com>
2022-12-01 22:18:04 -0500

    Listener function to respond to message

--- main.go
+++ main.go
@@ -7,15 +7,16 @@ import (
     // "io/ioutil"
     // "net/http"
     "os"
-    // "os/signal"
+    "os/signal"
     // "strings"
-    // "syscall"
+    "syscall"
 
     "github.com/bwmarrin/discordgo"
 		"github.com/joho/godotenv"
 )
 
 func main() {
+
 	// Load details from .env file
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -46,8 +47,32 @@ func main() {
 			return
 	}
 
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running. Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
 	// Cleanly close down the Discord session.
 	dg.Close()
 
-	fmt.Println("disconnected")
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	// Ignore all messages created by the bot itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if m.Content == "!bread" {
+
+		var err error
+		_, err = s.ChannelMessageSend(m.ChannelID, ":bread: get that bread :bread:")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+
 }
